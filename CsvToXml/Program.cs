7bdeebec927e19b91b93836daa8ce22ae6db700b
evy-goryunov <evy.goryunov@gmail.com>
2019-05-29 17:23:14 +0300

    1

---
+++ CsvToXml/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToXml
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StreamReader mainReader = new StreamReader("data.csv"); 
+		}
+	}
+}
